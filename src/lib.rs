ff955aa40e0435cd1a7da806b40de4645860c3cf
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-25 00:29:21 +0300

    add the line search to the run function

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@ use std::error::Error;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     
     Ok(())
 }
