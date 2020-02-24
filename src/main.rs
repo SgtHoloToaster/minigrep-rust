70cb2b7d68e4a3b4e90903e1a0011967608a435b
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-24 16:29:06 +0300

    extract the file reading logic into a separate function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,11 +12,17 @@ fn main() {
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file!");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text {}", contents);
+    println!("With text:\n{}", contents);
+    Ok(())
 }
 
 struct Config {
