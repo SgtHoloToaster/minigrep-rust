f36902bda14540893490a347c22d06ac681586cc
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-24 20:58:22 +0300

    remove redudant logging

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@ use std::error::Error;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
+    
     Ok(())
 }
 
