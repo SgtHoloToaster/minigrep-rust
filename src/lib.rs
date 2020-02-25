f720b01b5ff70667c749d27de2fd79415428d9d0
Mikhail Podolskiy <edge769@yandex.ru>
2020-02-25 22:26:11 +0300

    implement search_insensitive function

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,18 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let lowercase_query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&lowercase_query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 pub struct Config {
     pub query: String,
     pub filename: String
